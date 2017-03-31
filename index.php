00ae22a1f61cf2e472dfb19fe7687406ed7f1fb1
virtualbox <git@test.com>
2017-03-31 17:18:14 +0300

    20170331-1716

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
          <p> It works! </p>
 	    <a href="genpass.php">Генерация пароля, тест</a>
 
+<div class='pwd' style="width:200px;text-align: center; width=200px backgroun=green >    
+                     <?php include("genpass.php") ?>
+</div>
+
     </body>
 </html>
 
