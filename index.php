2e9e19bfbd8d8b4fc1c11fa78fde5b95b36206e1
zn33 <zn33@test.com>
2017-03-31 17:30:33 +0300

    zn33-1730

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 <html>
     <head>
 	<title>test</title>
+    <link href='style.css' rel='stylesheet' type='text/css'>
     </head>
     <body>
          <p> It works! </p>
 	<!--    <a href="genpass.php">Генерация пароля, тест</a> -->
 
-<div class='pwd' style="width:200px;text-align: center;" >    
+<div class='pwd'>    
                      <?php include("genpass.php") ?>
 </div>
 
     </body>
 </html>
-
